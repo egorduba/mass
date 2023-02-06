d59ba93fe9bcdc38d40307b1c682e312dc9f527e
egorduba <123907840+egorduba@users.noreply.github.com>
2023-02-06 22:04:31 +0200

    Create Дубчак.cs
    add Дубчак.cs

---
+++ Дубчак.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] mass = new int[8];
+            ////Задание 1
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    Console.Write($"mass[{i}]=");
+            //    mass[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    Console.WriteLine($"mass[{i}]={mass[i]}");
+            //}
+            //Задание 2
+            //double[] mass = new double[6] { -1.2, 3.05, 45, -13.5, 0.5, 1 };
+            //for(int i=0; i<mass.Length; i++)
+            //{
+            //    if (mass[i]>0)
+            //    {
+            //        Console.WriteLine(mass[i]);
+            //    }
+            //}
+            //Задание 3
+            //int[] mass = new int[6] { -7, 10, 5, 16, -4, 18 };
+            //int n = mass.Length;
+            //int k = n / 2;          
+            //int temp;
+            //for (int i = 0; i < k; i++)
+            //{
+            //    temp = mass[i];
+            //    mass[i] = mass[n - i - 1];
+            //    mass[n - i - 1] = temp;
+            //}
+            //foreach (int i in mass)
+            //{
+            //    Console.Write($"{i} \t");
+            //}
+            //Задание 4
+            //Random random = new Random();
+            //int h = random.Next(163, 190);
+            //int[] mass = new int[h];
+            //for(int i=0; i<h; i++)
+            //{
+            //    mass[i] = random.Next(163, 190);
+            //}
+            //foreach(int elem in mass)
+            //{
+            //    Console.WriteLine("{0}",elem);
+            //}
+            Console.ReadKey();
+
+        }
+
+    }
+}
